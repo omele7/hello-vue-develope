3850ab1b660113bc2a650985f8e6654eb9b6a885
Diego <u20211b293@gmail.com>
2024-08-29 16:15:58 -0500

    feat(welcoming): added register developer component.

---
+++ src/welcoming/components/register-developer.component.vue
@@ -0,0 +1,46 @@
+<script>
+export default {
+  name: "register-developer.component",
+
+  data() {
+    return {
+      firstName: '',
+      lastName: ''
+    }
+  },
+
+  methods: {
+    onSubmit() {
+      console.log('Registering Developer');
+      this.$emit('developer-registered', {
+        firstName: this.firstName,
+        lastName: this.lastName
+      });
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <h1>New Developer</h1>
+    <div>
+      <form @submit.prevent="onSubmit"></form>
+      <div>
+        <label for="firstName">First Name</label>
+        <input type="text" id="firstName" v-model="firstName" required/>
+      </div>
+      <div>
+        <label for="lastName">Last Name</label>
+        <input type="text" id="lastName" v-model="lastName" required/>
+      </div>
+      <div>
+        <button type="submit">Register</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
